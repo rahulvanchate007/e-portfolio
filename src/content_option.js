c66b0babce9b4c35e609ea774746dadbc3527b57
Rahul Vanchate <53507620+rahulvanchate007@users.noreply.github.com>
2024-01-21 22:49:53 -0500

    Add files via upload
    e-portfolio Initial Commit

---
+++ src/content_option.js
@@ -0,0 +1,136 @@
+const logotext = "RAHUL VANCHATE";
+const meta = {
+    title: "Rahul Vanchate",
+    description: "I’m Rahul Vanchate - Full stack devloper, currently studying at Old Dominion University",
+};
+
+const introdata = {
+    title: "I’m Rahul Vanchate",
+    animated: {
+        first: "Full Stack Web Developer",
+        second: "This is my E-Portfolio",
+    },
+    description: "Curently pursuing Master's in Computer Science at Old Dominion University, Norfolk, Virginia.",
+    your_img_url: "https://lh3.googleusercontent.com/pw/ABLVV84QU-_5-hAHkjTuOqAzoSP60n1mSOwGP1meEFV8_LqYabv8AfU-5V9-8PAOye4zxgBByIkXoBHSsHJ4D2NKBPUnFQF48WkwLFm3I5XV_de7moFa9tMqxX9_aWnxLXLRzyOhlw4v7jGBJL47ox5NzFGuCz3bgspTyWDFc8Xyw-C3PJGY2N7M9ZybbbJqNzEO-IjhdgCwpMgi9Tl7GhNpY3lRm7fquFW9VYUO8osPwweLSuQfyn8GNmLKFEWLnxEj7ZbGndFMKTkGN85qV7oPb8_jcRikwcJa5w1-xDEkvKk-IOBJDwQnJPO6fARiWu54dEM9kBCayD-dmWEWr6zd0JBqnrDmDaCyyy1lvIlt9CiHOf2HIOfwWObdoKPM8X167sjOzLPpf_EjfHhK0LDCi36SN79wlCmB8oJU1K-5PPuv62IUisEUeUSlcQ3CPKmWnlIrgloz6DkUGpbmDxI_WMkEjbtWxhjpt3rmet77IiXqjyxBleUmAtUk_RZ_AL7_UycjUphP4M_fo8uzijXzOm36oDB_qCsrYWNsS-ckFakC5c95X3NfjpQj88APY4hM7VljGwDzT6Cci7YQ8MKb95Unn4bSmgFQ7QnOWA-0bPLjDRtnScDsEVXwT746enB_0Qq-6fY_lU9JlyzZ7UlyySv0RfDOttfadZ5abl3s7vVB-FQdQJiIpcjIBjnmPlp-AkO8Ufg0WZ6ruWtR5frUZXjCW7BmhYQdJl4MnCDI1II-P0Bw0O2DXDp-bdEOiVt5QVwb4J4Uy0l2GIHwBM3yaphRsJJ-JosJrlSKRMvOlT0vrdDqW0Gz6Nr7Uci68-Ijbqk49lo4A8YMPN2173OrbICwerb8tCz2xJPOX9hYPa6jZIqPsGAu9RiHsalaJPC4o8PrC5wn4KNYNHPeyDbxdMT2WHmQmopxkpc_fGVAwuG8YkOx7NqsqK7DT-2owXmUHwZGqhcQhaxbxQ=w585-h878-s-no-gm?authuser=0",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "Software Developer with 4+ years of experience, trying to seek a challenging position and dedicate services to a reputed organization to gain better experience in life. Worked on technologies such as JAVA, MySQL, React JS etc. More specifics related to my work are as follows:" 
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "JAVA",
+        value: 90,
+    },
+    {
+        name: "MySQL",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 70,
+    },
+    {
+        name: "React",
+        value: 70,
+    },
+    {
+        name: "Spring Boot",
+        value: 85,
+    },
+    {
+        name: "Git",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Web Development",
+        description: "Capable of developing robust web applications with high-scalability level and stable maintenance. Flexible with modern day technologies and norms.",
+    },
+    {
+        title: "Internet of Things",
+        description: "Worked with multiple Internet of Things projects such as Automatic Irrigation System, Home Automation etc. Familiar with hardware such as Raspberry Pi, Arduino, ESP8266, PIC32 etc.",
+    },
+    {
+        title: "Database Administration",
+        description: "Familiar with managing and manipulating different relational and non-relational databases. Instances include MySQL, MSSQL, MongoDB.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://www.oyorooms.com/blog/wp-content/uploads/2018/03/proper-seating-arrangement.jpg",
+        description: "Meeting Room Booking System using Spring Boot",
+        link: "https://github.com/rahulvanchate007/MeetingRoomBookingSystem_using_SpringBoot",
+    },
+    {
+        img: "https://www.greenturf.com/wp-content/uploads/2016/05/sprinkler-system.jpg",
+        description: "Automatic Irrigation System using Internet of Things [IOT]",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1568667256531-7d5ac92eaa7a?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGlicmFyeSUyMGJvb2tzfGVufDB8fDB8fHww",
+        description: "Digital Library using Elasticsearch and ChatGPT chatbot.",
+        link: "https://github.com/rahulvanchate007/web-programming",
+    },
+    
+    {
+        img: "https://designnotes.blog.gov.uk/wp-content/uploads/sites/53/2020/06/Portfolio-Desk.jpg",
+        description: "E-Portfolio using React and SCSS",
+        link: "#",
+    },
+    {
+        img: "https://pikwizard.com/pw/medium/e3706c8342c570668c84833a1533ccce.jpg",
+        description: "Course Management System [Blackboard] using React",
+        link: "https://github.com/rahulvanchate007/Blackboard_Using_React",
+    },
+
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "rahulvanchate@gmail.com",
+    YOUR_FONE: "+1 (757)-470-8813",
+    description: "Please use the above details to get in contact with me regarding projects or collaborations. I would be delighted to be a part of change in the world of technology.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/RahulVanchate008",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/rahul-vanchate/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
